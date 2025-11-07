7b117f87f8fb849590389633442187edc5e6616b
Mihai Alexandru <alexandru-cristian.mihai@rabobank.nl>
2025-11-07 15:57:29 +0200

    Initial commit - Project setup

---
+++ AnimeBrowserChallenge/AnimeBrowserChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeBrowserChallengeApp.swift
+//  AnimeBrowserChallenge
+//
+//  Created by Alexandru Mihai on 07.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimeBrowserChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeBrowserChallengeTests/AnimeBrowserChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeBrowserChallengeTests.swift
+//  AnimeBrowserChallengeTests
+//
+//  Created by Alexandru Mihai on 07.11.2025.
+//
+
+import Testing
+@testable import AnimeBrowserChallenge
+
+struct AnimeBrowserChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
